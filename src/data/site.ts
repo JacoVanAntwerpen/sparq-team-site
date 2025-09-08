3fbf2785bd0c96836dd2a13749fcf278eb90af21
Jaco Van Antwerpen <z5115526@ad.unsw.edu.au>
2025-09-08 11:31:58 +1000

    Contact for: Update email r.daiyan@unsw.edu.au

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,6 +2,6 @@ export const SITE = {
   name: 'SparQ',
   tagline:
     'From feasibility studies to strategic roadmaps, we apply rigorous techno-economic modelling to low-carbon fuel and green metal pathways. Our insights equip industry to commercialise breakthrough technologies and provide governments with the policy frameworks to drive systemic decarbonisation',
-  email: 'hello@sparq.team',
+  email: 'r.daiyan@unsw.edu.au',
   address: 'UNSW Sydney, NSW, Australia',
 };
